21f3702298757364fc59718b470b3209877b0391
xy <xy_email2022@163.com>
2025-03-21 14:02:06 +0800

    添加展示二楼设备状态的页面

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,40 +5,45 @@ import Home from "./pages/Home";
 import Links from "./pages/Links";
 import Stats from "./pages/Stats";
 import Bucket from "./pages/Bucket";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import L2Links from "./pages/L2_Links";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
+    {
         path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/Home",
-        element: <Home />,
-      },
-      {
-        path: "/Links",
-        element: <Links />,
-      },
-      {
-        path: "/Stats",
-        element: <Stats />,
-      },
-      {
-        path: "/Bucket",
-        element: <Bucket />,
-      },
-    ],
-  },
+        element: <App/>,
+        children: [
+            {
+                path: "/",
+                element: <Home/>,
+            },
+            {
+                path: "/Home",
+                element: <Home/>,
+            },
+            {
+                path: "/Links",
+                element: <Links/>,
+            },
+            {
+                path: "/L2Links",
+                element: <L2Links/>,
+            },
+            {
+                path: "/Stats",
+                element: <Stats/>,
+            },
+            {
+                path: "/Bucket",
+                element: <Bucket/>,
+            },
+        ],
+    },
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+    <React.StrictMode>
+        <RouterProvider router={router}/>
+    </React.StrictMode>
 );
